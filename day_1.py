1955940a595232ead4931abb9565a14e90c4aeae
hapeitest <peihaowork@163.com>
2026-07-13 21:24:10 +0800

    add Day 1 Leetcode

---
+++ day_1.py
@@ -0,0 +1,12 @@
+# 给定一个整数数组 nums和一个整数目标值target，请你在该数组中找出和为目标值target的那两个整数，并返回它们的数组下标。
+# 你可以假设每种输入只会对应一个答案，并且你不能使用两次相同的元素。
+# 你可以按任意顺序返回答案。
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        hashtable = dict()
+        for i, num in enumerate(nums):
+            if target - num in hashtable:
+                return [hashtable[target - num], i]
+            hashtable[nums[i]] = i
+        return []
+ 
